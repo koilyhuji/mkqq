a54eb59c8f215a8300ea2ec8967ed2ca01855675
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-12-29 21:22:00 -0500

    .

--- app/src/main/java/mkqq/Controller/BookSearchController.java
+++ app/src/main/java/mkqq/Controller/BookSearchController.java
@@ -1,18 +1,77 @@
 package mkqq.Controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javafx.animation.TranslateTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import mkqq.BLL.BookBLL;
+import mkqq.BLL.BookIssueBLL;
+import mkqq.BLL.BookReturnBLL;
+import mkqq.DTO.BookDTO;
+import mkqq.DTO.BookIssueDTO;
+import mkqq.DTO.BookReturnDTO;
 import mkqq.MainApp;
 
 public class BookSearchController {
+
+    @FXML
+    private AnchorPane sch_root;
+    @FXML
+    private TextField bk_sch;
+    @FXML
+    private TableView<BookDTO> tbl_bk;
+
+    @FXML
+    private TableView<BookIssueDTO> tbl_issue;
+
+    @FXML
+    private TableView<BookReturnDTO> tbl_return;
+
+    private BookBLL bookBLL = new BookBLL();
+    private BookIssueBLL bookIssueBLL = new BookIssueBLL();
+    private BookReturnBLL bookReturnBLL = new BookReturnBLL();
+
+    private List<BookDTO> bookList = bookBLL.getBookDTOS();
+
+    public void initialize() {
+        setUpBookTable();
+        setUpIssueTable();
+        setUpReturnTable();
+
+        bk_sch.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                searchBooks(newValue);
+            }
+        });
+
+
+    }
+
+    private void searchBooks(String newValue) {
+    }
+
+    private void setUpReturnTable() {
+    }
+
+    private void setUpIssueTable() {
+    }
+
+    private void setUpBookTable() {
+    }
 
     public void img_bk(MouseEvent event) throws IOException {
         Stage stage;
